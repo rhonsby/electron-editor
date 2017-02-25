84d95b62999c394bb3585d095279701663c92e99
Robert Honsby <rhonsby@gmail.com>
2017-02-25 15:16:55 -0800

    Adds save ability

--- index.js
+++ index.js
@@ -1,12 +1,38 @@
-const { app, BrowserWindow } = require('electron');
+const { app, BrowserWindow, globalShortcut, ipcMain } = require('electron');
 const path = require('path');
+const fs = require('fs');
 
 
 let mainWindow;
 
 app.on('ready', () => {
     const indexPath = path.join(__dirname, 'index.html');
+    let currentOpenedFilePath;
 
     mainWindow = new BrowserWindow({});
     mainWindow.loadURL(`file:///${indexPath}`);
+
+    mainWindow.webContents.on('will-navigate', (e, url) => {
+        e.preventDefault();
+        mainWindow.webContents.send('navigate', url);
+    });
+
+    globalShortcut.register('CommandOrControl+S', () => {
+        mainWindow.webContents.send('ping', 'save');
+    });
+
+    ipcMain.on('async', (e, command, payload) => {
+        if (command === 'save') {
+            const { url, data } = payload;
+
+            fs.writeFile(url, data, 'utf8', (err) => {
+                if (err) {
+                    console.log(`Error processing ${url}`);
+                    return;
+                }
+
+                console.log(`Contents of ${url} saved!`);
+            })
+        }
+    })
 });
--- renderer.js
+++ renderer.js
@@ -1,12 +1,38 @@
+const { ipcRenderer } = require('electron');
 const monacoLoader = require('monaco-loader');
+const fs = require('fs');
 
 
 document.addEventListener('DOMContentLoaded', () => {
+    let currentFilePath;
+
     monacoLoader().then(() => {
-        monaco.editor.create(document.getElementById('container'), {
+        let editor = monaco.editor.create(document.getElementById('container'), {
             language: 'javascript',
             theme: 'vs-dark',
             automaticLayout: true,
+        });
+
+        ipcRenderer.on('navigate', (e, url) => {
+            url = url.slice(7);
+            fs.readFile(url, 'utf8', (error, result) => {
+                if (error) {
+                    console.log(`File error load failed: ${error}`)
+                    return;
+                }
+
+                editor.setModel(monaco.editor.createModel(result, 'javascript'));
+                currentFilePath = url;
+            });
+        });
+
+        ipcRenderer.on('ping', (e, command) => {
+            if (command === 'save') {
+                ipcRenderer.send('async', 'save', {
+                    url: currentFilePath,
+                    data: editor.getValue()
+                });
+            }
         });
     });
 });
